1a69a90bbb89e72e6675c9f5551a1614093862eb
HamzaDLM <hamzadellam@hotmail.com>
2021-12-13 12:28:40 +0100

    r'

--- trainticket.py
+++ trainticket.py
@@ -38,15 +38,20 @@ def reserve_ticket(date=go_date):
     time.sleep(sleep_duration)
     print(driver.page_source)
     time.sleep(sleep_duration)
+
+    a = driver.find_elements_by_xpath('//*[@id="root"]/section/div[1]/div[2]/main/div[1]/div/div/div/div/div[1]/div/div[1]/div[3]')
+    print("-----------------------", flush=True)
+    print(a, flush=True)
+    b = driver.find_elements_by_css_selector("#root > section > div.ant-row > div.ant-col > main > div.b_homeBanner > div > div > div > div > div.SearchWidget_form.home > div > div.sc-bwzfXH.cbJdAM > div:nth-child(3)")
+    print("-----------------------", flush=True)
+    print(b, flush=True)
+    c = driver.find_elements_by_class_name("sc-bxivhb")
+    print("-----------------------", flush=True)
+    print(c, flush=True)
+
     ################### STEP 1 
     # click reserve tab
     try:
-        a = driver.find_element_by_xpath('//*[@id="root"]/section/div[1]/div[2]/main/div[1]/div/div/div/div/div[1]/div/div[1]/div[3]')
-        print("-----------------------", flush=True)
-        print(a, flush=True)
-        b = driver.find_element_by_css_selector("#root > section > div.ant-row > div.ant-col > main > div.b_homeBanner > div > div > div > div > div.SearchWidget_form.home > div > div.sc-bwzfXH.cbJdAM > div:nth-child(3)")
-        print("-----------------------", flush=True)
-        print(b, flush=True)
         WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/section/div[1]/div[2]/main/div[1]/div/div/div/div/div[1]/div/div[1]/div[3]'))).click()
         # enter card type
         time.sleep(sleep_duration)
